c0349186384f2f947bed9d1b23ac3347ddeecd89
Visinho <Elvisvisinho@rocketmail.com>
2023-05-06 09:28:30 +0100

    Updated the delete property method

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -51,7 +51,9 @@ const createProperty = async (req, res) => {
 const getAllProperties = async (req, res) => {
 
     //adding sorting and filter logic 
-    const { _end, _order, _start, _sort, title_like = "", propertyType = "" } = req.body;
+    const { _end, _order, _start, _sort, title_like = "", propertyType = "" } = req.query;
+
+    const query = {};
 
     //checking if propertytype and title_like exist
     if(propertyType !== ""){
@@ -76,7 +78,7 @@ const getAllProperties = async (req, res) => {
 
     //passing total number of elements to the front end
     res.header("x-total-count", count);
-    res.header("Access-Control-Expose-Headers", x-total-count);
+    res.header("Access-Control-Expose-Headers", "x-total-count");
 
        res.status(200).json(properties);
     } catch (error) {
@@ -85,13 +87,42 @@ const getAllProperties = async (req, res) => {
 };
 
 
-const getPropertyDetail = async (req, res) => {};
+const getPropertyDetail = async (req, res) => {
+    const { id } = req.params;
+    const propertyExists = await Property.findOne({ _id: id}).populate("creator");
+
+    if(propertyExists) 
+    {res.status(200).json(propertyExists)}
+    else{
+       res.status(404).json({ message: "Property not found"});
+    }
+};
 
 
 const updateProperty = async (req, res) => {};
 
 
-const deleteProperty = async (req, res) => {};
+const deleteProperty = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const propertyToDelete = await Property.findById({_id:id}).populate("creator");
+        if(!propertyToDelete){
+            throw new Error("Property not found!");
+        }
+        const session = await mongoose.startSession();
+        session.startTransaction();
+
+        propertyToDelete.remove({session});
+        propertyToDelete.creator.allProperties.pull(propertyToDelete);
+
+        await propertyToDelete.creator.save({session});
+        await session.commitTransaction();
+
+        res.status(200).json({message: "Property deleted successfully"})
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+};
 
 
 export {getAllProperties, getPropertyDetail, createProperty, updateProperty, deleteProperty}; 
